ab70111ccd4cfd7da04c077030f008677d6059e8
Rekha Gaikwad <rekhag@quinnox.com>
2020-09-05 14:10:21 +0530

    first class

---
+++ BasicCoreJava/src/variablesAndDataTypes/DataTypes.java
@@ -0,0 +1,16 @@
+package variablesAndDataTypes;
+
+public class DataTypes {
+	
+	public static void main(String[] args) {
+		int age=33;
+		String name= "Rekha";
+		char gender='F';
+		boolean Marritalstatus=true;
+		double height = 5.4;
+		
+		System.out.println(age);
+				
+	}
+
+}
